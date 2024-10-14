4cc83d050bee2d548ae8e477a1785f3b7cf44258
Adam Narkonski <adam.narkonski@ajbell.co.uk>
2024-10-14 20:02:12 +0100

    tabs in vim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -69,7 +69,24 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.opt.scrolloff = 15
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+vim.opt.smartindent = true
+
+vim.opt.wrap = false
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
+vim.opt.undofile = true
+
+vim.opt.hlsearch = false
+vim.opt.incsearch = true
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
